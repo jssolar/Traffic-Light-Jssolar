b45237f5fae399cb7be5a562128a56aaec1caee9
Juan Solar <juansolarsolar@yahoo.com>
2023-06-14 17:38:20 -0400

    solo falta eliminar letras

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Semaforo from './components/Semaforo.jsx';
+
+
+function App() {
+  return (
+    <div className='Semaforo'>
+    <Semaforo  />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Semaforo.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import './App.css'
+
+const Semaforo = () => {
+    const [color, setColor] = useState("")
+    const botones = (color) => {
+        setColor(color)
+    }
+    
+
+
+console.log(color)
+    return(
+        <header className="semaforo-contenedor">
+            <div className="semaforo-luces">
+                <button   onClick = {() => botones('v')} className={color === 'v'?'encendido-verde': 'verde'}>{color}</button>
+                <button   onClick = {() => botones("a")}  className={color === 'a'?'encendido-amarillo': 'amarillo'}>{color}</button>
+                <button   onClick = {() => botones("r")} className={color ==='r'? 'encendido-rojo':'rojo'}>{color}</button>
+            </div>
+        </header>
+    )
+
+};
+
+export default Semaforo;
